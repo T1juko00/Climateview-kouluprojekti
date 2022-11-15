df3d11c23e2a773b7ac5e66f453deb9b1c36074d
Abdulah-Y <t1yuab00@students.oamk.fi>
2022-11-15 11:27:04 +0200

    login ja signin formit tehty

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,52 @@
 import React from 'react'
-
+import axios from 'axios'
+import Constants from './Constants.json'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function Login() {
+
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+
+    /*try {
+      const result = await axios.post(Constants.API_ADDRESS + '/loginForJWT, 
+      null,
+      {
+       auth: {
+        username: event.target.username.value,
+        password: event.target.password.value
+       }
+      });
+    console.log(result);
+    const receivedJWT = result.data.token;
+
+    navigate('/', { replace: true });
+
+    } catch (error) {
+    console.error(error);
+    }*/
+
+  }
+
     return (
         <div>
-            <h3>Login</h3>
+           <h2>Log in</h2>
+      <form onSubmit = { handleLoginSubmit }>
+        <div>
+          Username <br />
+          <input type="text" name= "username" />
+        </div>
+        <div>
+          Password <br />
+          <input type="text" name= "password" />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
             
         </div>
     )
